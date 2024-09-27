5b3b10a7fac0ab98bf951fa5bcfde8e16673030b
Saood Ahmad <saoodchoudhary9@gmail.com>
2024-09-27 11:22:39 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -39,14 +39,14 @@ const Navbar = ({ isScrolled }) => {
         </div>
 
         {/* Hamburger Icon for Mobile */}
-        <div className="md:hidden">
+        <div className="lg:hidden">
           <button onClick={toggleSidebar} className={`text-2xl ${isScrolled ? "text-[#7a634b]" : "text-white"} transition-colors duration-300`}>
             {isOpen ? <FiX /> : <FiMenu />}
           </button>
         </div>
 
         {/* Navbar for Desktop */}
-        <div className="hidden md:flex space-x-8">
+        <div className="hidden lg:flex lg:space-x-4 space-x-8">
           {navItems.map((item, ind) => (
             <Link
               key={ind}
@@ -59,7 +59,7 @@ const Navbar = ({ isScrolled }) => {
         </div>
 
         {/* Phone Number for Desktop */}
-        <div className="hidden md:flex items-center space-x-4">
+        <div className="hidden lg:flex items-center space-x-4">
           <Link
             href="tel:+919769241829"
             className={`flex items-center space-x-2 ${isScrolled ? "text-gray-800 hover:text-[#7a634b]" : "text-gray-200 hover:text-[#ffff]"} transition-all`}
@@ -71,7 +71,7 @@ const Navbar = ({ isScrolled }) => {
       </div>
 
       {/* Mobile Sidebar with Overlay */}
-      <div className={`${isOpen ? 'fixed inset-0 z-40' : 'hidden'} md:hidden`}>
+      <div className={`${isOpen ? 'fixed inset-0 z-40' : 'hidden'} lg:hidden`}>
         <div
           className="absolute inset-0 bg-black bg-opacity-50 z-30"
           onClick={toggleSidebar} // Close sidebar when clicking outside
@@ -80,7 +80,7 @@ const Navbar = ({ isScrolled }) => {
           initial={{ x: '-100%' }}
           animate={isOpen ? { x: 0 } : { x: '-100%' }}
           transition={{ type: 'spring', stiffness: 300, damping: 30 }}
-          className="fixed top-0 left-0 h-full w-64 bg-white shadow-xl z-40"
+          className="fixed top-0 left-0 h-full w-64 sm:w-96 bg-white shadow-xl z-40"
         >
           {/* Sidebar Content */}
           <div className="p-6 space-y-6">
